749a9d9421ef985fb4465f17f56b8974e4bbea8e
Ivaneminem <ivanbondarenko@knu.ua>
2024-11-13 09:54:43 +0200

    MKR1

---
+++ MKR1/MKR1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Читання даних із файлу INPUT.TXT
+        var lines = File.ReadAllLines("INPUT.TXT");
+        int n = int.Parse(lines[0]);
+        var orders = new (int Ti, int Ci)[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            var parts = lines[i + 1].Split();
+            int Ti = int.Parse(parts[0]);
+            int Ci = int.Parse(parts[1]);
+            orders[i] = (Ti, Ci);
+        }
+
+        // Сортуємо замовлення за винагородою у порядку спадання
+        orders = orders.OrderByDescending(order => order.Ci).ToArray();
+
+        // Масив для відмітки виконаних днів
+        bool[] days = new bool[1001];
+        int totalReward = 0;
+
+        // Проходимо через кожне замовлення
+        foreach (var order in orders)
+        {
+            // Знаходимо найближчий доступний день для виконання
+            for (int day = order.Ti; day > 0; day--)
+            {
+                if (!days[day])
+                {
+                    days[day] = true;
+                    totalReward += order.Ci;
+                    break;
+                }
+            }
+        }
+
+        // Записуємо результат у файл OUTPUT.TXT
+        File.WriteAllText("OUTPUT.TXT", totalReward.ToString());
+    }
+}
